c462846c67a45767b1f2f0f219b5f86adcfcc6eb
Takashi SUWA <bd.gfngfn@gmail.com>
2020-11-05 21:51:42 +0900

    initial commit

---
+++ src/rebar_sesterl_prv_compile.erl
@@ -0,0 +1,35 @@
+-module(rebar_sesterl_prv_compile).
+
+-behaviour(provider).
+-export([
+    init/1,
+    do/1,
+    format_error/1]).
+
+-define(PROVIDER, compile).
+-define(DEPS, [lock]).
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider =
+        providers:create([
+            {name, ?PROVIDER},
+            {module, ?MODULE},
+            {deps, ?DEPS},
+            {bare, true},
+            {short_desc, "Build Sesterl packages with rebar3"},
+            {desc, "Build Sesterl packages with rebar3"},
+            {example, "rebar3 compile"}
+        ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+    case rebar_utils:sh("sesterl ./ -o _generated/", [use_stdout, return_on_error]) of
+        {ok, _} -> rebar_prv_compile:do(State);
+        _       -> {error, "Failed to compile Sesterl package(s)"}
+    end.
+
+-spec format_error(Reason :: term()) -> iolist().
+format_error(Reason) ->
+    rebar_prv_compile:format_error(Reason).
