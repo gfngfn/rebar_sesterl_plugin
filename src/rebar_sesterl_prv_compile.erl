1a0e36135ebc198175f182571c4268bd4806d5fc
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-07-09 05:41:34 +0900

    separate 'rebar_sesterl_common' from 'rebar_sesterl_prv_compile'

--- src/rebar_sesterl_prv_compile.erl
+++ src/rebar_sesterl_prv_compile.erl
@@ -8,14 +8,14 @@
 -export([
     init/1,
     do/1,
-    format_error/1]).
+    format_error/1
+]).
 
 %%==============================================================================================
 %% Macros & Types
 %%==============================================================================================
 -define(PROVIDER, compile).
 -define(DEPS, [{default, lock}]).
--define(CONFIG_FILE_NAME, "package.yaml").
 
 %%==============================================================================================
 %% `provider' Callback Functions
@@ -38,38 +38,9 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    %% Deps :: [rebar_app_info.t()]
-    Deps = rebar_state:all_deps(State),
-    %% DepDirs :: [{string(), string()}]
-    DepDirs =
-        lists:filtermap(
-            fun(Dep) ->
-                NameBin = rebar_app_info:name(Dep),
-                NameStr = erlang:binary_to_list(NameBin),
-                Dir = rebar_app_info:dir(Dep),
-                case filelib:is_regular(Dir ++ "/" ++ ?CONFIG_FILE_NAME) of
-                    true  -> {true, {NameStr, Dir}};
-                    false -> false
-                end
-            end,
-            Deps),
-    lists:foreach(
-        fun({NameStr, Dir}) ->
-            rebar_api:info("Sesterl dependency: ~s (at: ~s)", [NameStr, Dir])
-        end,
-        DepDirs),
-    ExternalArg =
-        lists:append(
-            lists:map(
-                fun({NameStr, Dir}) ->
-                    " -p " ++ NameStr ++ ":" ++ Dir
-                end,
-                DepDirs)),
-    CommandLine = lists:flatten(io_lib:format("sesterl build ./~s", [ExternalArg])),
-    rebar_api:info("Compiling Sesterl programs (command: ~p) ...", [CommandLine]),
-    case rebar_utils:sh(CommandLine, [use_stdout, return_on_error]) of
-        {ok, _} -> rebar_prv_compile:do(State);
-        _       -> {error, "Failed to compile Sesterl package(s)"}
+    case rebar_sesterl_common:compile(State) of
+        ok -> rebar_prv_compile:do(State);
+        _  -> {error, "Failed to compile Sesterl package(s)"}
     end.
 
 -spec format_error(Reason :: term()) -> iolist().
